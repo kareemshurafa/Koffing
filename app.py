a9c345a75c6f4029089d398a6fa750db106e072b
kareemshurafa <kareemshurafa@gmail.com>
2023-11-14 20:35:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/home")
 def hello_world():
-    return "<h1 style='color:green'>Hello World!</h1>"
+    return "<h1 style='color:green'>Hello World! This is the final test!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
