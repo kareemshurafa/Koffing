074b4fc46cfe182296f6b4ebc15be524c14f51d2
haziqshahard <hhs21@ic.ac.uk>
2023-11-25 20:34:02 +0000

    Update app.py
    Establishing a heroku database connection

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template,request, json, url_for
 import requests
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://hvjmvqxxszylxg:3d1cdb2f1927cdb2ab1dc5e731015a768577b68f1907654be99a76127df98811@ec2-63-34-69-123.eu-west-1.compute.amazonaws.com:5432/dfuerbg1k2hvm2'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hvjmvqxxszylxg:3d1cdb2f1927cdb2ab1dc5e731015a768577b68f1907654be99a76127df98811@ec2-63-34-69-123.eu-west-1.compute.amazonaws.com:5432/dfuerbg1k2hvm2'
 
 @app.route("/home")
 def hello_world():
@@ -12,7 +21,6 @@ def hello_world():
 @app.route("/map")
 def mapview():
     return render_template('map.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
