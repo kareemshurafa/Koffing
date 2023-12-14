0c9484beb8d6110307aa46cf72de43a36613edf0
Am1rSy <amirsyahmibhari@gmail.com>
2023-12-14 17:53:24 +0000

    Added preliminary Widget API

--- app.py
+++ app.py
@@ -71,8 +71,12 @@ def hello_world():
     return "<h2 style='color:red'>Hello Koffing! This is the final test!</h2>"
 
 @app.route("/map")
-def mapview():
+def aqiview():
     return render_template('map.html')
+
+@app.route("/aqi")
+def mapview():
+    return render_template('aqi_widget.html')
 
 @app.route("/signup")
 def signupview():
