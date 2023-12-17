c3c679a88f9eba2f35913e5e03b5b97457177f37
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2023-12-17 14:52:12 +0300

    Further Updated Database

--- app.py
+++ app.py
@@ -10,8 +10,49 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import flash
 
+ENV = "dev"
+
+
+if ENV == "dev":
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hvjmvqxxszylxg:3d1cdb2f1927cdb2ab1dc5e731015a768577b68f1907654be99a76127df98811@ec2-63-34-69-123.eu-west-1.compute.amazonaws.com:5432/dfuerbg1k2hvm2'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Koffing123!' #Should likely hide this
+
+db = SQLAlchemy(app)
+
+#Initialize database
+
+
+class UserData(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique=True)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+    password = db.Column(db.String(20),nullable=True)
+    addy = db.Column(db.String(500), nullable = True)
+    DOB = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    #User can have many logs
+    #logs = db.relationship('AsthmaLog', backref ='log')
+    #To call info from the user data, you would do log.name, log.email, etc
+
+    def __init__(self, name, email, date_added, password, addy, DOB):
+        self.name = name
+        self.email = email
+        self.date_added = date_added
+        self.password = password 
+        self.addy = addy  
+        self.DOB = DOB
+    
+    def __repr__(self):
+        return '<User %r>' % self.id
 
 @app.route('/database/test', methods = ['GET', 'POST'] ) #Double check these methods
 def add_user():
@@ -56,7 +97,7 @@ def testlog():
 def add_users():
     name = None
     form = UserForm()
-    
+        
     #Validate form
     if form.validate_on_submit(): 
         name = form.name.data
@@ -66,6 +107,22 @@ def add_users():
     
     return render_template("add_user.html",
                            form = form)
+
+@app.route('/test')
+def index():
+    return render_template('test.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        inhaler = request.form['inhaler']
+        # print(customer,inhaler)
+        if customer =='' or inhaler=='':
+            return render_template('test.html', message='Please enter required fields')
+
+
+        return "<h2 style='color:red'>Yipee!</h2>"
 
 #Create a form class
 class AsthmaLogForm(FlaskForm):
