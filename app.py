d719a74cf949359ef5d4c29fc20c4c3c360ba01f
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2023-12-26 10:17:54 +0300

    Implementing unit testing

--- app.py
+++ app.py
@@ -30,46 +30,68 @@ db = SQLAlchemy(app)
 
 #Initialize database
 
-class TestModel(db.Model):
-    __tablename__ = 'testmodel' 
+class GPDetails(db.Model):
+    __tablename__ = 'GPDetails'
     id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(200), nullable = False) #Someone can have the same name lol
-    inhaler =db.Column(db.String(200),nullable = False)
+    GPname = db.Column(db.String(200),nullable=False)
+    GPcode = db.Column(db.String(30),nullable=False)
+    address = db.Column(db.String(200), nullable = False)
+    GPnum = db.Column(db.Integer(),nullable=True) #Set to unique=True?
 
-    def __init__(self,name,inhaler):
-        self.name = name
-        self.inhaler = inhaler 
+    def __init__(self,GPname,GPcode,address,GPnum):
+        self.GPname = GPname
+        self.GPcode = GPcode
+        self.address = address
+        self.GPnum = GPnum
+
+class UserDetails(db.Model):
+    __tablename__ = 'UserDetails'
+    id = db.Column(db.Integer,primary_key = True)
+    firstname = db.Column(db.String(30),nullable = False)
+    surname = db.Column(db.String(30),nullable = False)
+    email = db.Column(db.String(50),nullable=False, unique=True) 
+    phonenum = db.Column(db.Integer,nullable=True,unique=True)
+    dob = db.Column(db.DateTime,default=datetime.utcnow)
+    address = db.Column(db.String(50),nullable=True)
+    #Creating a one-to-many relationship to puffhistory
+        #backref - gives email when puff.email is called
+        #lazy = True - load the data as necessary 
+    asthmapuffs = db.relationship('PuffHistory',backref='email',lazy=True)
     
-    def __repr__(self):
-        return '<User %r>' % self.id
+    #Linking GP to user - One to One relationship
+    gpid = db.Column(db.Integer,db.ForeignKey(GPDetails.id))
+    #Not 100% sure that this actually queries GPDetails.id
 
-# class UserData(db.Model):
-#     __tablename__ = 'feedback'
-#     id = db.Column(db.Integer,primary_key = True)
-#     name = db.Column(db.String(200), nullable = False)
-#     email = db.Column(db.String(120), nullable = False, unique=True)
-#     date_added = db.Column(db.DateTime, default = datetime.utcnow)
-#     password = db.Column(db.String(20),nullable=True)
-#     addy = db.Column(db.String(500), nullable = True)
-#     DOB = db.Column(db.DateTime, default=datetime.utcnow)
+    def __init__(self,firstname,surname,email,phonenum,dob,address):
+        self.firstname = firstname
+        self.surname = surname
+        self.email = email
+        self.phonenum = phonenum
+        self.dob = dob
+        self.address = address
+
+
+
+class PuffHistory(db.Model):
+    __tablename__='PuffHistory'
+    id = db.Column(db.Integer,primary_key = True)
+    inhalertype = db.Column(db.String(40),nullable=False)
+    medname = db.Column(db.String(40),nullable=False)
+    dosageamt = db.Column(db.Integer,nullable=False)
+    puffno = db.Column(db.Integer,nullable=False)
+    datetaken = db.Column(db.DateTime,default=datetime.utcnow)
+    timetaken = db.Column(db.DateTime,default=datetime.utcnow) #NEED TO MAKE SURE JUST TIME
     
-#     #User can have many logs
-#     #logs = db.relationship('AsthmaLog', backref ='log')
-#     #To call info from the user data, you would do log.name, log.email, etc
+    def __init__(self,inhalertype,medname,dosageamt,puffno,datetaken,timetaken):
+        self.inhalertype = inhalertype
+        self.medname = medname
+        self.dosageamt=dosageamt
+        self.puffno=puffno
+        self.datetaken = datetaken
+        self.timetaken = timetaken
 
-#     def __init__(self, name, email, date_added, password, addy, DOB):
-#         self.name = name
-#         self.email = email
-#         self.date_added = date_added
-#         self.password = password 
-#         self.addy = addy  
-#         self.DOB = DOB
-    
-#     def __repr__(self):
-#         return '<User %r>' % self.id
-
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.route('/database/test', methods = ['GET', 'POST'] ) #Double check these methods
 def add_user():
