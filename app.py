316c5dcee382d5051f67dad052caf3f74301983c
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2023-12-29 15:56:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ import flash
 # from .models import GPDetails, UserDetails, PuffHistory
 from .extensions import db
 
-from flask import Flask
-from .extensions import db
-
 def create_app(database_URI = 'postgresql://hvjmvqxxszylxg:3d1cdb2f1927cdb2ab1dc5e731015a768577b68f1907654be99a76127df98811@ec2-63-34-69-123.eu-west-1.compute.amazonaws.com:5432/dfuerbg1k2hvm2'):
     app = Flask(__name__)
 
