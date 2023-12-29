4f88a0a6f5827a331d00b69bcd0e704f40b25cad
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2023-12-30 00:12:04 +0300

    Implementing login

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from datetime import datetime
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import models
+from .extensions import db
 # from .models import GPDetails, UserDetails, PuffHistory
 
-db=SQLAlchemy()
+
 
 def create_app(database_URI = 'postgresql://hvjmvqxxszylxg:3d1cdb2f1927cdb2ab1dc5e731015a768577b68f1907654be99a76127df98811@ec2-63-34-69-123.eu-west-1.compute.amazonaws.com:5432/dfuerbg1k2hvm2'):
     app = Flask(__name__)
--- models.py
+++ models.py
@@ -1,5 +1,23 @@
-from .app import db
+from .extensions import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class LoginDetails(db.Model):
+    __tablename__ = 'LoginDetails'
+    email = db.Column(db.String(50), unique=True,primary_key = True) #Need to implement a check if this exists or not
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
 
 class GPDetails(db.Model):
     __tablename__ = 'GPDetails'
@@ -20,7 +38,7 @@ class UserDetails(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     firstname = db.Column(db.String(30),nullable = False)
     surname = db.Column(db.String(30),nullable = False)
-    email = db.Column(db.String(50),nullable=False, unique=True) 
+    email = db.Column(db.String(50),nullable=False, unique=True)  
     phonenum = db.Column(db.Integer,nullable=True,unique=True)
     dob = db.Column(db.DateTime,default=datetime.utcnow)
     address = db.Column(db.String(50),nullable=True)
