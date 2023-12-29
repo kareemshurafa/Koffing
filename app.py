28fc5d87dc9df116091f9620712b6881a6b01dd1
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2023-12-29 17:40:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def create_app(database_URI = 'postgresql://hvjmvqxxszylxg:3d1cdb2f1927cdb2ab1dc
     return app
 
 bp = Blueprint("main", __name__)
-# app = create_app()
+app = create_app()
 
 @bp.route("/home")
 def home():
