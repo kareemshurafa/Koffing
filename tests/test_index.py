c3baeffa1077376fa5cb6e906017dbfdb95dfbf4
haziqshahard <146861553+haziqshahard@users.noreply.github.com>
2024-01-02 18:57:10 +0300

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -5,7 +5,7 @@ def test_home(client):
     
     # assert b"<h2 style='color:red'>Hello Koffing! This is the final test!</h2>" in response.data
     assert b'<img id="Asthma" src="Asthma.png" srcset="Asthma.png 1x, Asthma@2x.png 2x">' in response.data
-    #assert used to check if it exists
+    #assert used to check if it exists  
     #Do form and database submission in this as well
 
     
