df4b28eef6a5d78b3528518cf83f65f71466e2a5
kareemshurafa <kareemshurafa@gmail.com>
2024-01-03 20:25:10 +0000

    Added logout button

--- app.py
+++ app.py
@@ -201,6 +201,10 @@ def updateview():
 def index():
     return render_template('test.html')
 
+@bp.route('/logout')
+def logoutview():
+    return redirect("/")
+
 # @bp.route('/submit', methods=['POST'])
 # def submit():
 #     if request.method == 'POST':
