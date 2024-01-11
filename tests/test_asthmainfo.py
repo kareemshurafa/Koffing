da9090ef5e695d4743f0cb749920502bb944cae5
Am1rSy <amirsyahmibhari@gmail.com>
2024-01-11 16:27:49 +0000

    Updated unit test for Map, home and Asthmainfo

---
+++ tests/test_asthmainfo.py
@@ -0,0 +1,12 @@
+def test_asthmainfo(client):
+    response = client.get("/asthmainfo")
+    info = response.data.decode() #decode binary to string
+    
+    #Test if Logbook link exist
+    assert b'<div id="My_Logbook_">' in response.data
+    
+    assert "Asthma is a chronic respiratory disease affecting individuals of all ages. " in info
+    assert "Koffing is a web-based app that offers a novel approach to asthma tracking. " in info
+    
+    #Test if page exist
+    assert response.status_code == 200
---
+++ tests/test_mapinfo.py
@@ -0,0 +1,21 @@
+def test_mapinfo(client):
+    response = client.get("/mapinfo")
+    # Test for the presence of the "My Logbook" button
+    assert b'<div id="My_Logbook_">' in response.data
+    
+    # Test to ensure the Air Quality Map loads correctly
+    assert b'<div id="Air_Quality_Map">' in response.data
+    
+    # Test to ensure the map related script is loaded
+    assert b'src="https://maps.googleapis.com/maps/api/js' in response.data
+    
+    # Test for the presence of the AQI widget
+    assert b'<div class="aqi-widget" id="aqiWidget">' in response.data
+    
+    # Test for the presence and accessibility of Settings
+    assert b'<div id="Settings__">' in response.data
+    
+    # Test for the presence of the chart
+    assert b'<div id = "aqiChart">' in response.data
+    
+    assert response.status_code == 200
