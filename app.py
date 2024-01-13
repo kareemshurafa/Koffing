569b47272196132e000d724ec9b748e4f77f8d1d
haziqshahard <hhs21@ic.ac.uk>
2024-01-13 14:53:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ def signuppost():
         if request.method == 'GET' and session['logged_in'] == True:
             return redirect("/home")
     except:
-        return redirect("/login")
+        return render_template("Sign_up_page_template.html")
     
 ### Test on the form submission and visualisation in template ###
 
