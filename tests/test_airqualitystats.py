c81e1fffd30abd3473d99e82cbe6d5416ba7501e
isilsusokmen <116863287+isilsusokmen@users.noreply.github.com>
2024-01-14 15:34:10 +0000

    updated test_airqualitystats.py

--- tests/test_airqualitystats.py
+++ tests/test_airqualitystats.py
@@ -2,5 +2,14 @@ def test_airqualitystats(client):
     response = client.get("/airqualitystats")
     info = response.data.decode() #decode binary to string
 
+    assert response.status_code == 200
+
+    #checking for specific HTML elements 
+    assert '<div id="Air_Quality_Stats">' in info  # Check for main div id
+    assert '<div id="My_Logbook_">' in info  # Check for Logbook link
+    assert '<svg class="Icon_awesome-user-alt"' in info  # Check for SVG icon
+
+
+
     
     
